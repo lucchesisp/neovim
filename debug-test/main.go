fe2d46aeb1965b7977b6250371532d9c952a89d4
Gabriel Lucchesi <lucchesisp@gmail.com>
2026-01-18 02:40:22 -0300

    test: add goroutines to main.go for debugger testing
    - Add worker function with channels
    - Create 3 concurrent worker goroutines
    - Test job distribution and result collection
    - Add select example with multiple channels
    - Use time.Sleep for demonstration of concurrent execution

--- debug-test/main.go
+++ debug-test/main.go
@@ -1,6 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func worker(id int, jobs <-chan int, results chan<- int) {
+	for j := range jobs {
+		fmt.Printf("Worker %d started job %d\n", id, j)
+		time.Sleep(time.Second)
+		results <- j * 2
+		fmt.Printf("Worker %d finished job %d\n", id, j)
+	}
+}
 
 func main() {
 	i := 10
@@ -8,5 +20,51 @@ func main() {
 
 	result := sum(x, i)
 	fmt.Println("Result:", result)
-	// awesome comment!
+
+	// Test goroutines
+	jobs := make(chan int, 100)
+	results := make(chan int, 100)
+
+	// Start 3 workers
+	for w := 1; w <= 3; w++ {
+		go worker(w, jobs, results)
+	}
+
+	// Send 5 jobs
+	for j := 1; j <= 5; j++ {
+		jobs <- j
+	}
+	close(jobs)
+
+	// Collect results
+	for a := 1; a <= 5; a++ {
+		<-results
+	}
+
+	fmt.Println("All jobs completed!")
+
+	// Test with select and channels
+	ch1 := make(chan string)
+	ch2 := make(chan string)
+
+	go func() {
+		time.Sleep(1 * time.Second)
+		ch1 <- "from ch1"
+	}()
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		ch2 <- "from ch2"
+	}()
+
+	for i := 0; i < 2; i++ {
+		select {
+		case msg1 := <-ch1:
+			fmt.Println("Received:", msg1)
+		case msg2 := <-ch2:
+			fmt.Println("Received:", msg2)
+		}
+	}
+
+	fmt.Println("Done with channels!")
 }
